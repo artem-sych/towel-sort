849b42dc2fbc744e3aa92c82c2b7d564fb7f4b70
unknown <artem.sych1989@gmail.com>
2022-01-31 08:31:08 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -5,15 +5,23 @@ module.exports =
     function towelSort(matrix) {
         let arr = [];
         let arrNumb = [];
+
         matrix.forEach((subArr, index) => {
+
+
             if (index % 2 == 0) {
                 arr.push(subArr);
+
             } else {
                 let subArrReverc = subArr.reverse();
                 arr.push(subArrReverc);
+
             }
         });
         arr.forEach(subArr => {
+            if (subArr.length === 0) {
+                return [];
+            }
             subArr.forEach(el => {
                 arrNumb.push(el);
             });
